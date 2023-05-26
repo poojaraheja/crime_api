7010e5351e499a4769ca9158e50fa7ec7308a9b3
corbinbot <viveklawaniya98@gmail.com>
2023-05-26 21:14:00 +0530

    c

--- records.php
+++ records.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers:*');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   include 'DbConnect.php';
   $user = json_decode(file_get_contents('php://input'));
   // $email = $user->email;
